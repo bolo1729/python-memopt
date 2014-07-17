38af2f20742de146f3f29c7b25a523729f8faaf1
Lukasz Bolikowski <l.bolikowski@icm.edu.pl>
2014-07-17 10:42:46 +0200

    Initial data

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='memopt',
+		version='0.0.1',
+		description='Memory-efficient data structures',
+		author='Lukasz Bolikowski',
+		author_email='l.bolikowski@icm.edu.pl',
+		maintainer='Lukasz Bolikowski',
+		maintainer_email='l.bolikowski@icm.edu.pl',
+		url='https://github.com/bolo1729/python-memopt',
+		packages=['memopt'],
+		classifiers=[
+			'Development Status :: 4 - Beta',
+			'Intended Audience :: Developers',
+			'License :: OSI Approved :: MIT License',
+			'Operating System :: OS Independent',
+			'Programming Language :: Python',
+			'Topic :: Utilities',
+			],
+		license="MIT",
+		)
